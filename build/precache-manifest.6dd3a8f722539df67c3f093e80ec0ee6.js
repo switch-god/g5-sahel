a88b6802f64e935392bb2373001f0b1f6c4f53e3
Enis Ayéchi <5796052-switch-god@users.noreply.gitlab.com>
2020-04-13 19:13:01 +0100

    BuildMode

---
+++ build/precache-manifest.6dd3a8f722539df67c3f093e80ec0ee6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab4334da4eb0ed4480ad9e9b9bd162a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13c56b92e28f9f1382e2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c22694a1752e8a1e0f93",
+    "url": "/static/css/main.b52f97b9.chunk.css"
+  },
+  {
+    "revision": "13c56b92e28f9f1382e2",
+    "url": "/static/js/2.9a3e6fbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a3e6fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22694a1752e8a1e0f93",
+    "url": "/static/js/main.f16518a2.chunk.js"
+  },
+  {
+    "revision": "90b31a62a95dfda14822",
+    "url": "/static/js/runtime-main.db7796ee.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
